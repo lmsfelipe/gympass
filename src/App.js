2a121bdf8a21bfde6e087b51d2e83254825de63b
Felipe Lima <lms.felipe@gmail.com>
2018-10-06 19:31:29 -0300

    Requesting repos from github api

--- src/App.js
+++ src/App.js
@@ -2,33 +2,23 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { requestRepo } from './store/repositories';
-
-
+import { getRepos } from './store/repositories';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <button onClick={() => this.props.requestRepo('some data')}>Redux Test</button>
+          <button onClick={() => this.props.getRepos()}>Redux Test</button>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ requestRepo }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ getRepos }, dispatch);
 
 export default connect(null, mapDispatchToProps)(App);
--- src/store/repositories/index.js
+++ src/store/repositories/index.js
@@ -1,26 +1,72 @@
+import ServiceHelper from "../../services/service-helper";
+import { API_URL, TOKEN } from "../../services/configs";
+
 /**
  * Actions
  */
 
-export const requestRepo = data => ({
-  type: 'REQUEST_REPO',
+export const requestRepos = () => ({
+  type: 'REQUEST_REPOS'
+});
+
+export const rejectRepos = data => ({
+  type: 'REJECT_REPOS',
   data
 });
+
+export const fulfillRepos = data => ({
+  type: 'FULFILL_REPOS',
+  data
+});
+
+/**
+ * Thunks
+ */
+
+ export const getRepos = () => async dispatch => {
+   try {
+     dispatch(requestRepos());
+     const response = await ServiceHelper.SendGet(`${API_URL}/users/lmsfelipe/repos`, TOKEN);
+     dispatch(fulfillRepos(response));
+   } catch (error) {
+     dispatch(rejectRepos(error.response));
+   }
+ }
 
 /**
  * Reducer
  */
 
 export const initialState = {
-  repoResponse: {}
+  loading: false,
+  error: false,
+  errorMessage: null,
+  reposResponse: {}
 };
 
 const repositoriesReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'REQUEST_REPO':
+    case 'REQUEST_REPOS':
       return {
         ...state,
-        repoResponse: action.data
+        loading: true,
+        error: false
+      };
+    
+    case 'REJECT_REPOS':
+      return {
+        ...state,
+        loading: false,
+        error: true,
+        errorMessage: action.data
+      };
+
+    case 'FULFILL_REPOS':
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        reposResponse: action.data
       };
 
     default:
